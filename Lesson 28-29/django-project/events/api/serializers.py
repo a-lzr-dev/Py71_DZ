5d6b352e441dd1a617d8abeb087955c3b5cf45c3
Alexander <developer.alexander.lazerko@gmail.com>
2026-05-21 12:06:11 +0300

    Add Lesson 28-29_DZ

--- Lesson 28-29/django-project/events/api/serializers.py
+++ Lesson 28-29/django-project/events/api/serializers.py
@@ -11,32 +11,3 @@ class EventSerializer(serializers.ModelSerializer):
         model = Event
         fields = ['id', 'name', 'meeting_time', 'description', 'users']
         read_only_fields = ['id']
-
-"""
-class UserShortSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email"]
-
-
-class EventSerializer(serializers.ModelSerializer):
-#    user = UserShortSerializer(read_only=True)
-#    tags = serializers.ListSerializer(child=serializers.CharField(), read_only=True)
-#    image = serializers.CharField(required=False)
-
-    class Meta:
-        model = Event
-        fields = ["id", "name", "meeting_time", "description"]
-#        read_only_fields = ["id", "user", "created_at", "updated_at"]
-
-
-#class EventListSerializer(serializers.ModelSerializer):
-#    user = UserShortSerializer(read_only=True)
-#    tags = serializers.ListSerializer(child=serializers.CharField(), read_only=True)
-#    description_preview = serializers.CharField(read_only=True)
-
-#    class Meta:
-#        model = Event
-#        fields = ["id", "name"]
-
-"""
